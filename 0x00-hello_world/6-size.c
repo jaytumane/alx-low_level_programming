a78622615b9bad08710243fc2b13b6365582e4a9
jako tumane <jaydecota@gmail.com>
2022-12-08 14:18:43 -0800

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 
 	printf("size of int: %ld bytes\n",sizeof(integerType));
         printf("size of float: %ld bytes\n",sizeof(floatType));
-        printf("size of double: %ld bytes\n",sizeof(doublelType));
+        printf("size of double: %ld bytes\n",sizeof(doubleType));
 	printf("size of char: %ld bytes\n",sizeof(charType));
 
 	return (0);
