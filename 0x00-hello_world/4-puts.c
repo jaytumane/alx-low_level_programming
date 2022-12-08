6dd31867d482decef0bebbb5b2734cbd2236afb2
jako tumane <jaydecota@gmail.com>
2022-12-08 13:41:26 -0800

    puts6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - entry
  *
- * Return : Always 0 (success)
+ * Return : (success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
