8581e34a2189b16427d28e5875c070ee56d932b1
jako tumane <jaydecota@gmail.com>
2022-12-08 14:48:36 -0800

    sixe of variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int main(void)
 	float floatType;
 	double doubleType;
 
-	printf("size of char: %ld byte(s)\n",sizeof(charType));
-        printf("size of int: %ld byte(s)\n",sizeof(integerType));
-        printf("size of long int: %ld byte(s)\n",sizeof(floatType));
-	printf("size of long long int: %ld byte(s)\n",sizeof(doubleType));
-        printf("size of float: %ld byte(s)\n",sizeof(floatType));
+	printf("size of a char: %ld byte(s)\n",sizeof(charType));
+        printf("size of an int: %ld byte(s)\n",sizeof(integerType));
+        printf("size of a long int: %ld byte(s)\n",sizeof(floatType));
+	printf("size of a long long int: %ld byte(s)\n",sizeof(doubleType));
+        printf("size of a float: %ld byte(s)\n",sizeof(floatType));
 
 	return (0);
 }
