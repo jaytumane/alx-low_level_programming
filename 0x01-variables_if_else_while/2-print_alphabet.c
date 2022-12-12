425f8c3996957a60513ff165b543e368c490fc87
jako tumane <jaydecota@gmail.com>
2022-12-12 12:33:15 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int x = 97, y = 122;
+
+	while (x <= y)
+	{
+		putchar(x);
+		x++;
+	}
+
+	putchar('\n');
+	return (0);
+}
