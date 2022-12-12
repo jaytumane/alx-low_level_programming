8d08948a705f755a8bcd547702fb395865de2f85
jako tumane <jaydecota@gmail.com>
2022-12-12 12:58:00 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		if (x < 9)
+		{
+			printf(", ");
+		}
+		x++;
+	}
+	printf("\n");
+	return (0);
+}
