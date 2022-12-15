9fde75ddd8442d10db59cbc0862d69f84403d652
jako tumane <jaydecota@gmail.com>
2022-12-15 04:32:52 -0800

    like commemnt subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - makes an infinity loop
+ *
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+
+	/**
+	 *while (i < 10)
+	 *{
+	 *	putchar(i);
+	 *}
+	 */
+
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
