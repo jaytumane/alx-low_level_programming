26a002fa5e8aab756b98e018ae2fab022ac1f7e5
jako tumane <jaydecota@gmail.com>
2022-12-21 09:55:30 -0800

    98 betery

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - restets n to 98
+ * @n: number to be updated
+ *
+ * Return: nothing to return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
